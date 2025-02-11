5678c8026ded322d202b0acf8fb5873cf25d3363
Fellipe de Oliveira <fellipe.oli.p@gmail.com>
2025-02-10 23:12:02 -0300

    exercicios de SET realizados

---
+++ Exercicios-Set/Exercicio1.js
@@ -0,0 +1,39 @@
+// Exerc. 1 -  Criar, verificar, adicionar e remover item do SET
+
+let numeros = new Set([1, 2, 3, 4 ,5]);
+console.log(numeros.size);
+
+numeros.add(5);
+console.log(numeros);
+
+numeros.delete(1);
+console.log(numeros);
+
+// Exerc. 2 - Converta um array para Set 
+
+let frutas = ['maça', 'banana', 'maça', 'banana', 'laranja'];
+let frutasUnicas = [new Set(frutas)];
+console.log(frutasUnicas);
+
+
+// Exerc. 3 - Criar um Set de 1 a 10, fazer um forEach para exibir os valores e verificiar se o 7 está presente.
+
+let numbers = new Set([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+numbers.forEach((number) => {
+    console.log(`Número: ${number}`);
+    
+})
+
+console.log(numbers.has((7)));
+
+// Exerc. 4 - Excreva uma função p/ remover valores duplicados de um array usando set.
+
+function removerDuplicatas(array) {
+    return Array.from(new Set(array));
+}
+
+let nomes = ['José', 'Gabriel', 'Gilson', 'Ronaldo', 'José', 'Gilson'];
+let nomesUnicos = removerDuplicatas(nomes);
+console.log(nomesUnicos);
+
+
